a97a0e15e5052eb96527eda9f73f8495fbd43c65
amanscetprojects <109068140+amanscetprojects@users.noreply.github.com>
2022-07-12 18:56:11 +0530

    Update initialize.php

--- initialize.php
+++ initialize.php
@@ -1,10 +1,10 @@
 <?php
 $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url','http://madnispring.herokuapp.com//');
+if(!defined('base_url')) define('base_url','https://madnispring.herokuapp.com/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
-if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
-if(!defined('DB_NAME')) define('DB_NAME',"company_website_db");
+if(!defined('DB_SERVER')) define('DB_SERVER',"sql110.epizy.com");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"epiz_32129928");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"Ri7q3NqtYe");
+if(!defined('DB_NAME')) define('DB_NAME',"epiz_32129928_company_website_db");
 ?>
